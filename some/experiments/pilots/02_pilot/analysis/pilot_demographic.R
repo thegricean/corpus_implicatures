529eb480029aa333e90767518b60903c7c01ee4a
Leyla <lkursat@stanford.edu>
2019-02-26 16:56:38 -0800

    added pilot experiments

---
+++ some/experiments/pilots/02_pilot/analysis/pilot_demographic.R
@@ -0,0 +1,35 @@
+library(tidyverse)
+d = read.csv("pilot_demographic.csv", header = TRUE)
+
+# look at comments
+unique(d$comments)
+
+# fair price
+ggplot(d, aes(x=fairprice)) +
+  geom_histogram(stat="count")
+
+# overall assessment
+ggplot(d, aes(x=asses)) +
+  geom_histogram(stat="count")
+table(d$asses)
+
+# enjoyment (3 levels)
+ggplot(d, aes(x=enjoyment)) +
+  geom_histogram(stat="count")
+
+# age
+ggplot(d, aes(x=age)) +
+  geom_histogram(stat="count")
+
+# gender
+ggplot(d, aes(x=gender)) +
+  geom_histogram(stat="count")
+
+# education
+ggplot(d, aes(x=education)) +
+  geom_histogram(stat="count")
+
+# language
+ggplot(d, aes(x=language)) +
+  geom_histogram(stat="count")
+
